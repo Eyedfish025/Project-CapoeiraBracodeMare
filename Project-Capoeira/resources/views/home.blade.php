d1b0dc1b4a15c40b0026e6c108a85d21073cf85a
Eyedfish025 <jonatasgustavo24@gmail.com>
2025-12-12 13:36:10 -0300

    home

---
+++ Project-Capoeira/resources/views/home.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Capoeira Braço de Maré</title>
+</head>
+<body>
+    <nav>
+        <h1>Bem-vindo à Capoeira Braço de Maré</h1>
+        <ul>
+            <li><a href="/home">Início</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+    </nav>
+    <div>
+        <h2>Conheça nosso grupo de Capoeira Angola: Braço de Maré</h2>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem iure incidunt cum, 
+            voluptatem dolorem pariatur atque porro! Ut nam delectus labore assumenda fugit? Eius reprehenderit 
+            fugit laboriosam odit, debitis atque.
+        </p>
+        <div>
+            <h3>Nossos Mestres</h3>
+            <div>
+                <h4>Mestre Gigante</h4>
+                <img src="" alt="Mestre Gigante">
+                <p>Mestre Gigante formado e graduado na capoeira Angola, com mais de X anos de experiencia</p>
+            </div>
+            <div>
+                <h4>Mestre Melque</h4>
+                <img src="" alt="Mestre Melque">
+                <p>Mestre Melque formado e graduado na capoeira Angola, com mais de X anos de experiencia</p>
+            </div>
+            <div>
+                <h4>Mestre Pastinha</h4>
+                <img src="" alt="Mestre Pastinha">
+                <p>Mestre Pastinha formado e graduado na capoeira Angola, com mais de X anos de experiencia</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
